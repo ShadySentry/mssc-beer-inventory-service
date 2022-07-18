2ad7664909b4e18901dcda832e76382603c1f9a7
ShadySentry <eurohawk@gmail.com>
2022-07-18 23:06:29 +0300

    zero on hands fix

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
@@ -59,8 +59,12 @@ public class AllocationServiceImpl implements AllocationService {
                 beerOrderLine.setQuantityAllocated(allocatedQty + inventory);
                 beerInventory.setQuantityOnHand(0);
 
+            }
+
+            if(beerInventory.getQuantityOnHand()==0){
                 beerInventoryRepository.delete(beerInventory);
             }
+
         });
 
     }
